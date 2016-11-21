a752e065b9326a20968cefdf8281e97303b107d5
ugurdonmez <ugurdonmez87@gmail.com>
2016-11-21 23:05:49 +0100

    cookie service added.

---
+++ services/cookie-service.js
@@ -0,0 +1,11 @@
+angular.module('kinder-app').service('CookieService', function ($cookies) {
+    
+    this.addToCookie = function(key,value) {
+        $cookies.put(key, value);
+    };
+    
+    this.getFromCookie = function(key) {
+        return $cookies.get(key);
+    };
+    
+});
