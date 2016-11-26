24b988b130bec4cfa09d58f535a39b7f2b419ae1
ugurdonmez <ugurdonmez87@gmail.com>
2016-11-26 21:31:38 +0100

    admin navigation translations added.

--- common/directive/app-navigation/app-navigation.js
+++ common/directive/app-navigation/app-navigation.js
@@ -1,10 +1,32 @@
-angular.module('kinder-app').directive('appNavigation', function ($rootScope, $location) {
+angular.module('kinder-app').directive('appNavigation', function ($rootScope, $location, TranslationService) {
     return {
         restrict: 'E',
         replace: true,
         scope: {},
         templateUrl: 'common/directive/app-navigation/app-navigation.html',
         link: function (scope, element, attrs, fn) {
+            
+            TranslationService.getTranslation(scope, 'tr');
+            
+            scope.currentMenuItem = "list-schools";
+
+            var getCurrentMenuItem = function (currentPath) {
+                if (currentPath.indexOf("list-schools") !== -1) {
+                    return "list-schools";
+                } else if (currentPath.indexOf("add-school") !== -1) {
+                    return "add-school";
+                } else if (currentPath.indexOf("update-school") !== -1) {
+                    return "update-school";
+                } else {
+                    return "";
+                }
+			};
+            
+            scope.$on('$locationChangeSuccess', function(event, data) {
+                scope.currentMenuItem = getCurrentMenuItem(data);
+            });
+
+            scope.currentMenuItem = getCurrentMenuItem($location.path());
         }
     };
 });
